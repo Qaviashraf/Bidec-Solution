32da853ed387fc612816366b5a35452011298343
YamanAamir <136349726+YamanAamir@users.noreply.github.com>
2024-07-22 03:48:47 -0700

    finalizing

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import projectImg1 from '../assets/ProjectAssets/Mask group.svg'
+import projectImg1 from '../assets/ProjectAssets/Mask group1.svg'
+import projectImg2 from '../assets/ProjectAssets/Mask group.svg'
+import projectImg3 from '../assets/ProjectAssets/Mask group3.svg'
+import projectImg4 from '../assets/ProjectAssets/Mask group2.svg'
 import rectangle from '../assets/ProjectAssets/Rectangle 31.svg'
 import bgimg from '../assets/ProjectAssets/Ellipse 21.svg'
+import arrow from '../assets/footerAssets/arrow.svg'
 function Projects() {
     return (
         <div className="pb-10 my-10">
             <div
-                className="heading flex justify-center lg:justify-around items-center flex-col md:flex-row gap-20 my-10 px-20">
+                className="heading flex justify-center lg:justify-around items-center flex-col lg:flex-row gap-20 my-10 px-20">
                 <span
                     data-aos="fade-right"
                     className="lg:text-6xl text-4xl lg:w-1/2 w-1/2 relative">
@@ -18,10 +22,9 @@ function Projects() {
                     </span>
                     have been done
                 </span>
-                <div
-                    data-aos="fade-down" className="relative">
-                    {/* <img className='' src={bgimg} alt="" /> */}
-                    <ul className='absolute bottom-[-10vh] right-0 pl-20'>
+                <div data-aos="fade-down" className="relative">
+                    <ul
+                        className='absolute bottom-[-10vh] lg:right-[10vw] pl-20'>
                         <li className='font-sans bottom-12 right-[1vw] rotate-[350deg] absolute font-semibold text-md rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 w-36'>UI/UX design</li>
                         <li className='font-sans bottom-20 rotate-[35deg] left-0 absolute font-semibold text-md rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 w-40'>graphics design</li>
                         <li className='font-sans bottom-[8vw] rotate-[320deg] left-[-5vw] absolute font-semibold text-md rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 '>development</li>
@@ -29,6 +32,90 @@ function Projects() {
                         <li className='font-sans bottom-0 rotate-0 left-1 absolute font-semibold text-md rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 w-36'>Web design</li>
                     </ul>
                 </div>
+                <img className='absolute w-screen right-0' src={bgimg} alt="" />
+            </div>
+            <div className="flex flex-col justify-center items-center py-10 gap-10">
+                <div className="flex justify-around flex-col xl:flex-row items-center gap-10">
+                    <div
+                        data-aos="fade-up"
+                    >
+                        <div className="relative">
+                            <img className='' src={projectImg1} alt="" />
+                            <button className='absolute right-0 bottom-3 z-10 '>
+                                <img className='p-3 rounded-full bg-[#227C85]' src={arrow} alt="" />
+                            </button>
+                        </div>
+                        <div>
+                            <h1 className='text-4xl py-5'>E-commerce Website
+                                Design</h1>
+                            <p className='flex gap-5 divide-x divide-gray-300 text-gray-400 text-lg'>
+                                <span className='px-5 font-sans'>UI/UX Design</span>
+                                <span className='px-5 font-sans'>Wireframe</span>
+                                <span className='px-5 font-sans'>Prototype</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div
+
+                        data-aos="fade-up" className="xl:pt-32 pt-0 ">
+                        <div className="relative">
+                            <img src={projectImg2} alt="" />
+                            <button className='absolute right-0 bottom-3 z-10 '>
+                                <img className='p-3 rounded-full bg-[#227C85]' src={arrow} alt="" />
+                            </button>
+                        </div>
+                        <div>
+                            <h1 className='text-4xl py-5'>E-commerce Website
+                                Design</h1>
+                            <p className='flex gap-5 divide-x divide-gray-300 text-gray-400 text-lg'>
+                                <span className='px-5 font-sans'>UI/UX Design</span>
+                                <span className='px-5 font-sans'>Wireframe</span>
+                                <span className='px-5 font-sans'>Prototype</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-around flex-col xl:flex-row items-center gap-10">
+                    <div
+                        data-aos="fade-up">
+                        <div className="relative">
+                            <img src={projectImg3} alt="" />
+                            <button className='absolute right-0 bottom-3 z-10 '>
+                                <img className='p-3 rounded-full bg-[#227C85]' src={arrow} alt="" />
+                            </button>
+                        </div>
+                        <div>
+                            <h1 className='text-4xl py-5'>E-commerce Website
+                                Design</h1>
+                            <p className='flex gap-5 divide-x divide-gray-300 text-gray-400 text-lg'>
+                                <span className='px-5 font-sans'>UI/UX Design</span>
+                                <span className='px-5 font-sans'>Wireframe</span>
+                                <span className='px-5 font-sans'>Prototype</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        data-aos="fade-up" className="xl:pt-32 pt-0 ">
+                        <div className="relative">
+                            <img src={projectImg4} alt="" />
+                            <button className='absolute right-0 bottom-3 z-10 '>
+                                <img className='p-3 rounded-full bg-[#227C85]' src={arrow} alt="" />
+                            </button>
+                        </div>
+                        <div>
+                            <h1 className='text-4xl py-5'>E-commerce Website
+                                Design</h1>
+                            <p className='flex gap-5 divide-x divide-gray-300 text-gray-400 text-lg'>
+                                <span className='px-5 font-sans'>UI/UX Design</span>
+                                <span className='px-5 font-sans'>Wireframe</span>
+                                <span className='px-5 font-sans'>Prototype</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <button className='bg-[#227C85] p-3 px-5 rounded-full text-white font-sans'>View All Projects</button>
             </div>
         </div>
     )
--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import { TiPlus } from "react-icons/ti";
+import React, { useState } from 'react';
+import { TiPlus, TiMinus } from "react-icons/ti";
+
 function Question() {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const toggleAccordion = (index) => {
+        setActiveIndex(activeIndex === index ? null : index);
+    };
+
+    const faqs = [
+        {
+            question: 'What services does your digital agency offer?',
+            answer: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever'
+        },
+        {
+            question: 'What industries do you specialize in?',
+            answer: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever'
+        },
+        {
+            question: 'What is your process for working with clients?',
+            answer: 'Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry\'s standard dummy text ever'
+        }
+    ];
+
     return (
         <div 
-        data-aos="fade-up"
-        className="flex justify-center items-start pt-20 p-10 md:px-20 gap-10 flex-col">
+            data-aos="fade-up"
+            className="flex justify-center items-start pt-20 p-10 md:px-20 gap-4 flex-col bg-gray-50"
+        >
             <h1 className='md:text-6xl text-4xl md:px-10 p-0'>Frequently asked questions</h1>
-            <div
-        data-aos="fade-right" className="flex justify-between  w-full items-center border-2 border-gray-400 rounded-xl">
-                <h1 className='md:text-4xl text-lg p-4 md:p-10'>What services does your digital agency offer?</h1>
-                <span className='md:text-4xl text-2xl px-4'>
-                    <button><TiPlus /></button>
-                    {/* <button><TiPlus /></button> */}
-                </span>
-            </div>
-            <div
-        data-aos="fade-right" className="flex justify-between w-full items-center border-2 border-gray-400 rounded-xl">
-                <h1 className='md:text-4xl text-lg p-4 md:p-10'>What industries do you specialize in?</h1>
-                <span className='md:text-4xl text-2xl px-4'>
-                    <button><TiPlus /></button>
-                    {/* <button><TiPlus /></button> */}
-                </span>
-            </div>
-            <div
-        data-aos="fade-right" className="flex justify-between  w-full items-center border-2 border-gray-400 rounded-xl">
-                <h1 className='md:text-4xl text-lg p-4 md:p-10'>what is your process for working with clients</h1>
-                <span className='md:text-4xl text-2xl px-4'>
-                    <button><TiPlus /></button>
-                    {/* <button><TiPlus /></button> */}
-                </span>
-            </div>
+            {faqs.map((faq, index) => (
+                <div
+                    key={index}
+                    data-aos="fade-right"
+                    className="w-full mb-2"
+                >
+                    <div
+                        className={`flex justify-between items-center rounded-xl cursor-pointer ${activeIndex === index ? 'bg-[#227c85] text-white rounded-b-none' : 'bg-white text-black'} ${activeIndex === index ? '' : 'border border-gray-400'}`}
+                        onClick={() => toggleAccordion(index)}
+                    >
+                        <h1 className='md:text-4xl text-lg p-4 md:p-6'>{faq.question}</h1>
+                        <span className='md:text-4xl text-2xl px-4'>
+                            {activeIndex === index ? <TiMinus /> : <TiPlus />}
+                        </span>
+                    </div>
+                    {activeIndex === index && (
+                        <div className="p-4 bg-[#227c85] text-white rounded-b-xl">
+                            <p className='font-sans'>{faq.answer}</p>
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default Question
+export default Question;
