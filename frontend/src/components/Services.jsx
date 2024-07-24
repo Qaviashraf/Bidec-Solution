c09d72f1be58aba9776a4a9778a4f51134acf4be
Qaviashraf <126185428+Qaviashraf@users.noreply.github.com>
2024-07-24 06:20:47 -0700

    Update Services

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -26,22 +26,22 @@ function Services() {
                 className="flex flex-col gap-5 px-3 md:w-1/3">
                     <div className=' border-b border-gray-400 group relative' >
                         <h1 className='text-4xl group-hover:text-[#227C85]' >01<span className='sm:ps-8 ps-2'>Branding </span></h1>
-                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 right-[-10vh] top-[-5vh] transition-opacity duration-500' src={brandingImg} alt="" />
+                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 w-20 md:w-52 right-0 top-0 md:left-[-140px] transition-opacity duration-500' src={brandingImg} alt="" />
                         <p className='font-sans lg:ps-20 ps-5 text-lg py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
                     </div>
                     <div className=' border-b border-gray-400 group relative'>
                         <h1 className='text-4xl group-hover:text-[#227C85]'>02<span className='sm:ps-8 ps-2' >Web Design </span></h1>
-                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 left-20 top-10 transition-opacity duration-500' src={designing} alt="" />
+                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 w-20 md:w-52 right-0 top-0 md:left-[-140px] transition-opacity duration-500' src={designing} alt="" />
                         <p className='font-sans lg:ps-20 ps-5 text-lg py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
                     </div>
                     <div className=' border-b border-gray-400 group relative'>
                         <h1 className='text-4xl group-hover:text-[#227C85]'>03<span className='sm:ps-8 ps-2'>Motion Graphic </span></h1>
-                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 right-20 top-[5vh] transition-opacity duration-500' src={graphics} alt="" />
+                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 w-20 md:w-52 right-0 top-0 md:left-[-140px] transition-opacity duration-500' src={graphics} alt="" />
                         <p className='font-sans lg:ps-20 ps-5 text-lg py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
                     </div>
                     <div className=' border-b border-gray-400 group relative'>
                         <h1 className='text-4xl group-hover:text-[#227C85]'>04<span className='sm:ps-8 ps-2'>UI/UX </span></h1>
-                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 left-0 top-20 transition-opacity duration-500' src={UiUx} alt="" />
+                        <img className='opacity-0 group-hover:opacity-100 absolute z-10 w-20 md:w-52 right-0 top-0 md:left-[-140px] transition-opacity duration-500' src={UiUx} alt="" />
                         <p className='font-sans lg:ps-20 ps-5 text-lg py-5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
                     </div>
                 </div>
