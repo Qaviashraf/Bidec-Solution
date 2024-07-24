efbd41f26e9e4e3789f9d4ff4451b4c2f084b752
YamanAamir <136349726+YamanAamir@users.noreply.github.com>
2024-07-24 04:31:38 -0700

    Add changes

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -7,6 +7,7 @@ import rectangle from '../assets/ProjectAssets/Rectangle 31.svg'
 import bgimg from '../assets/ProjectAssets/Ellipse 21.svg'
 import arrow from '../assets/footerAssets/arrow.svg'
 import bgShade from '../assets/HeroSection/Ellipse 18.svg'
+import '../App.css'
 
 import { Link } from 'react-router-dom'
 
@@ -15,40 +16,43 @@ function Projects() {
         <div className="pb-10 px-3 py-5">
             <img className='absolute left-0' src={bgShade} alt="" />
             <div
-                className="heading flex justify-center text-center md:text-evenly lg:justify-around items-center flex-col md:flex-row gap-20 md:my-10 md:px-20">
+                className="heading flex justify-center text-center md:text-evenly lg:justify-between items-center flex-col md:flex-row gap-20 md:my-10 md:px-20">
                 <span
                     data-aos="fade-right"
-                    className="lg:text-6xl text-4xl lg:w-[60%] w-full relative">
+                    className="md:text-6xl text-4xl lg:w-[60%] w-full relative">
                     Our
                     <span className="relative inline-block">
-                        <span className="invisible">spac</span>
-                        <img className="bg-green-950 rounded-full absolute top-0 left-1 lg:left-4 lg:h-12 h-8" src={rectangle} alt="" />
+                        <span className="invisible">spce</span>
+                        <img className="bg-green-950 rounded-full absolute top-0 left-4 md:h-12 h-8" src={rectangle} alt="" />
                         <span className=""> Project  </span>
                     </span><br />
                     have been done
                 </span>
                 <div
-                    data-aos="fade-down" className="relative hidden lg:block">
+                    data-aos="fade-down" className="hidden lg:block">
 
-                    <ul className='absolute bottom-[-10vh] right-[12vh] pl-20 hover:bg-gray-300'>
+                    <ul className='relative top-24 right-32 xl:right-[250px] pl-20 hover:bg-gray-300'>
                         <Link to='service/branding' className='font-sans bottom-10 right-[1vw] rotate-[350deg] absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>UI/UX design</Link>
                         <Link to='service/branding' className='font-sans bottom-20 rotate-[25deg] left-1 absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2 w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>graphics design</Link>
-                        <Link to='service/web-design-development' className='font-sans bottom-[8vw] rotate-[330deg] left-[-8vw] absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2  w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>development</Link>
+                        <Link to='service/web-design-development' className='font-sans top-[-150px] rotate-[330deg] right-16 absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2  w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>development</Link>
                         <Link to='service/branding' className='font-sans bottom-16 rotate-[50deg] left-28 absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2  w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>brand design</Link>
-                        <Link to='service/web-design-development' className='font-sans bottom-0 rotate-0 left-4 absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2  w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>Web design</Link>
+                        <Link to='service/web-design-development' className='font-sans bottom-2.5 rotate-0 left-4 absolute text-lg rounded-full bg-[#227C851A] border-2 border-[#FFFFFF] px-5 py-2  w-48 text-center hover:bg-[#227C85] hover:text-white hover:border-[#227C85]'>Web design</Link>
                     </ul>
                 </div>
 
-                <img className='absolute z-[-10] right-0' src={bgimg} alt="" />
+                <img className='absolute z-[-10] right-2.5' src={bgimg} alt="" />
             </div>
             <div
                 data-aos="fade-up"
                 className="flex flex-col gap-10  pt-10">
                 <div className="flex justify-center  lg:flex-row flex-col gap-10 items-center">
                     <div className='flex flex-col justify-center items-center'>
-                        <div className="relative">
-                            <img src={projectImg1} alt="" />
-                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-0 bottom-0 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
+                        <div className="relative projectBgImg" >
+                            <div className="img">
+
+                            </div>
+                            {/* <img src={projectImg1} alt="" /> */}
+                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-2.5 bottom-2.5 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
                         </div>
                         <h1 className='text-4xl pt-8'>E-commerce Website
                             Design</h1>
@@ -59,9 +63,12 @@ function Projects() {
                         </p>
                     </div>
                     <div className='flex flex-col lg:pt-32 p-0 justify-center items-center'>
-                        <div className="relative">
-                            <img src={projectImg2} alt="" />
-                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-0 bottom-0 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
+                        <div className="relative projectBgImg" >
+                            <div className="img">
+
+                            </div>
+                            {/* <img src={projectImg2} alt="" /> */}
+                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-2.5 bottom-2.5 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
                         </div>
                         <h1 className='text-4xl pt-8'>E-commerce Website
                             Design</h1>
@@ -78,9 +85,12 @@ function Projects() {
                 className="flex justify-center gap-10 items-center">
                 <div className="flex justify-center  lg:flex-row flex-col gap-10 items-center">
                     <div className='flex flex-col justify-center items-center'>
-                        <div className="relative">
-                            <img src={projectImg3} alt="" />
-                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-0 bottom-0 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
+                        <div className="relative projectBgImg" >
+                            <div className="img">
+
+                            </div>
+                            {/* <img src={projectImg3} alt="" /> */}
+                            <button className='absolute bg-[#227C85] m-4 hover:bg-[#165056] duration-300 right-2.5 bottom-2.5 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
                         </div>
                         <h1 className='text-4xl pt-8'>E-commerce Website
                             Design</h1>
@@ -91,9 +101,12 @@ function Projects() {
                         </p>
                     </div>
                     <div className='flex flex-col lg:pt-32 p-0 justify-center items-center'>
-                        <div className="relative">
-                            <img src={projectImg4} alt="" />
-                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-0 bottom-0 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
+                        <div className="relative projectBgImg" >
+                            <div className="img">
+
+                            </div>
+                            {/* <img src={projectImg4} alt="" /> */}
+                            <button className='absolute bg-[#227C85] hover:bg-[#165056] duration-300 right-2.5 bottom-2.5 rounded-full z-10 p-4'> <img className='' src={arrow} alt="" /></button>
                         </div>
                         <h1 className='text-4xl pt-8'>E-commerce Website
                             Design</h1>
@@ -105,12 +118,12 @@ function Projects() {
                     </div>
                 </div>
             </div>
-            
+
             <div
                 data-aos="fade-up" className="flex justify-center pt-10">
                 <button className='font-sans hover:bg-[#165056] duration-300 text-lg bg-[#227C85] p-3 px-7 text-white rounded-full '>View All Projects</button>
             </div>
-            
+
         </div>
     )
 }
